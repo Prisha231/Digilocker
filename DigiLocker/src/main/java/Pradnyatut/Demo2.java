257e90b9d37471654be96c800d7f7c7b43ef0034
DELL <DELL@DESKTOP-92MCULV>
2025-01-25 12:28:21 +0530

    cretated demo2 class.

---
+++ DigiLocker/src/main/java/Pradnyatut/Demo2.java
@@ -0,0 +1,16 @@
+package Pradnyatut;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+	System.out.println("welcome to pradnyas era");
+    System.out.println("hello raj");
+	}
+
+}
